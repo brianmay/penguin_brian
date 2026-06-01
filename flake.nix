af8f034c0bda17379528bff5181d6f2c7daa16db
Brian May <brian@linuxpenguins.xyz>
2026-06-02 07:21:34 +1000

    chore: update to nixpkgs 26.05

--- flake.nix
+++ flake.nix
@@ -1,43 +1,48 @@
 {
   description = "Brian's website";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flockenzeit.url = "github:balsoft/flockenzeit";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    flockenzeit,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      flockenzeit,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      version = builtins.substring 0 8 self.lastModifiedDate;
-      build = {
-        BUILD_DATE = with flockenzeit.lib.splitSecondsSinceEpoch {} self.lastModified; "${F}T${T}${Z}";
-        VCS_REF = "${self.rev or "dirty"}";
-      };
+        version = builtins.substring 0 8 self.lastModifiedDate;
+        build = {
+          BUILD_DATE = with flockenzeit.lib.splitSecondsSinceEpoch { } self.lastModified; "${F}T${T}${Z}";
+          VCS_REF = "${self.rev or "dirty"}";
+        };
 
-      build_toml = (pkgs.formats.toml {}).generate "build.toml" build;
-    in {
-      packages.default = pkgs.stdenv.mkDerivation {
-        name = "penguin_brian-${version}";
-        src = ./.;
-        buildPhase = ''
-          cp ${build_toml} build.toml
-          ${pkgs.zola}/bin/zola build
-        '';
-        installPhase = ''
-          mkdir -p $out
-          cp -r public/* $out
-        '';
-      };
-      devShells.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.zola
-        ];
-      };
-    });
+        build_toml = (pkgs.formats.toml { }).generate "build.toml" build;
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "penguin_brian-${version}";
+          src = ./.;
+          buildPhase = ''
+            cp ${build_toml} build.toml
+            ${pkgs.zola}/bin/zola build
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp -r public/* $out
+          '';
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.zola
+          ];
+        };
+      }
+    );
 }
